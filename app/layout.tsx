5edbbe3359ddb54b061284291a55b4a35afe4e66
Farid Vatani <faridvatani@gmail.com>
2024-07-01 15:14:47 +0330

    feat: Add tRPC integration and configuration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
+import { TRPCProvider } from "@/components/ui/trpc-provider";
 import { cn } from "@/lib/utils";
 import "./globals.css";
 
@@ -26,7 +27,9 @@ export default function RootLayout({
           fontSans.variable,
         )}
       >
+        <TRPCProvider>
         {children}
+        </TRPCProvider>
       </body>
     </html>
   );
--- db/schema.ts
+++ db/schema.ts
@@ -2,6 +2,7 @@ import { uuid, text, timestamp, pgTable } from "drizzle-orm/pg-core";
 
 export const users = pgTable("users", {
   id: uuid("id").primaryKey().defaultRandom(),
+  name: text("name").notNull(),
   email: text("email").notNull().unique(),
   username: text("username").notNull().unique(),
   password: text("password").notNull(),
