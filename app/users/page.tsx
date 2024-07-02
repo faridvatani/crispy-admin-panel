03787e9788f0a6b35bcefc6de336b2667b62fb90
Farid Vatani <faridvatani@gmail.com>
2024-07-02 17:52:27 +0330

    feat: Implement Users Table and fetching Users from DB

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,12 +1,82 @@
+"use client";
+
+import { trpc } from "@/server/client";
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
 export default function Users() {
+  const users = trpc.users.get.useQuery();
+
   return (
     <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
       <div className="flex items-center">
         <h1 className="text-lg font-semibold md:text-2xl">Users</h1>
       </div>
-      <div>
-        page content
-      </div>
+      <Card>
+        <CardContent>
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead>ID</TableHead>
+                <TableHead className="hidden sm:table-cell">Name</TableHead>
+                <TableHead className="hidden sm:table-cell">Email</TableHead>
+                <TableHead className="hidden sm:table-cell">Username</TableHead>
+                <TableHead className="hidden md:table-cell">Date</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {users.status === "pending" && (
+                <TableRow>
+                  <TableCell colSpan={5}>Loading...</TableCell>
+                </TableRow>
+              )}
+
+              {users.status === "error" && (
+                <TableRow>
+                  <TableCell colSpan={5}>
+                    Error: {users.error.message}
+                  </TableCell>
+                </TableRow>
+              )}
+
+              {users.status === "success" && users.data?.length === 0 && (
+                <TableRow>
+                  <TableCell colSpan={5}>No users found</TableCell>
+                </TableRow>
+              )}
+
+              {users.data?.map((user) => (
+                <TableRow key={user.id}>
+                  <TableCell>
+                    <div className="hidden text-sm text-muted-foreground md:inline">
+                      {user.id}
+                    </div>
+                  </TableCell>
+                  <TableCell className="hidden sm:table-cell">
+                    {user.name}
+                  </TableCell>
+                  <TableCell className="hidden sm:table-cell">
+                    {user.email}
+                  </TableCell>
+                  <TableCell className="hidden sm:table-cell">
+                    {user.username}
+                  </TableCell>
+                  <TableCell className="hidden md:table-cell">
+                    {new Date(user.createdAt).toLocaleDateString()}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </CardContent>
+      </Card>
     </main>
   );
 }
